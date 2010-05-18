121fe344744dbf2fe2b91c51b8b3807a7a2fbd7c
alisdair sullivan <alisdairsullivan@yahoo.ca>
2010-05-18 12:16:25 -0700

    first working version

---
+++ src/jsx_common.hrl
@@ -0,0 +1,57 @@
+-record(opts, {
+    comments = false,
+    escaped_unicode = ascii
+}).
+
+%% whitespace
+-define(space, 16#20).
+-define(tab, 16#09).
+-define(cr, 16#0D).
+-define(newline, 16#0A).
+
+%% object delimiters
+-define(start_object, 16#7B).
+-define(end_object, 16#7D).
+
+%% array delimiters
+-define(start_array, 16#5B).
+-define(end_array, 16#5D).
+
+%% kv seperator
+-define(comma, 16#2C).
+-define(quote, 16#22).
+-define(colon, 16#3A).
+
+%% string escape sequences
+-define(escape, 16#5C).
+-define(rsolidus, 16#5C).
+-define(solidus, 16#2F).
+-define(formfeed, 16#0C).
+-define(backspace, 16#08).
+-define(unicode, 16#75).
+
+%% math
+-define(zero, 16#30).
+-define(decimalpoint, 16#2E).
+-define(negative, 16#2D).
+-define(positive, 16#2B).
+
+%% comments
+-define(star, 16#2a).
+
+-define(is_hex(Symbol),
+    (Symbol >= $a andalso Symbol =< $z); (Symbol >= $A andalso Symbol =< $Z); 
+        (Symbol >= $0 andalso Symbol =< $9)
+).
+
+-define(is_nonzero(Symbol),
+    Symbol >= $1 andalso Symbol =< $9
+).
+
+-define(is_noncontrol(Symbol),
+    Symbol >= ?space
+).
+
+-define(is_whitespace(Symbol),
+    Symbol =:= ?space; Symbol =:= ?tab; Symbol =:= ?cr; Symbol =:= ?newline
+).
