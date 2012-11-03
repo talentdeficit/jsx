c73327f2cb63f02668800bdf08290716345d728e
Yurii Rashkovskii <yrashk@gmail.com>
2012-11-02 23:49:20 -0700

    Add expm package files

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "jsx", description: "an erlang application for consuming, producing and manipulating json",
+                 version: "1.3.3", keywords: ["json","erlang"], 
+                 maintainers: [[name: "alisdair sullivan", 
+                                email: "alisdairsullivan@yahoo.ca"]],
+                 repositories: [[github: "talentdeficit/jsx", tag: "v1.3.3"]])
+    
