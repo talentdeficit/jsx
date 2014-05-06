29d07b242dc1bcb2836b0a6a2d0759024e97dd1c
alisdair sullivan <alisdairsullivan@yahoo.ca>
2014-05-06 19:05:06 +0000

    bump to 2.0.2 and publish to hex.pm

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jsx (v2.0.1) #
+# jsx (v2.0.2) #
 
 an erlang application for consuming, producing and manipulating [json][json]. 
 inspired by [yajl][yajl]
---
+++ package.exs
@@ -0,0 +1,29 @@
+defmodule JSX.Mixfile do
+use Mix.Project
+
+  def project do
+    [
+      app: :jsx,
+      version: "2.0.2",
+      description: "an erlang application for consuming, producing and manipulating json. inspired by yajl",
+      package: package
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        "CHANGES.md",
+        "LICENSE",
+        "package.exs",
+        "README.md",
+        "rebar.config",
+        "rebar.config.script",
+        "config",
+        "src"
+      ],
+      contributors: ["alisdair sullivan"],
+      links: [{"github", "https://github.com/talentdeficit/jsx"}]
+    ]
+  end
+end
